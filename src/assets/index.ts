5997bda4e9988d3b13647f8dbaebf8aa487f2cf7
Marcin Barc <marb637@gmail.com>
2024-07-09 22:33:30 +0200

    Menu positioned in the upper right corner

---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+import Shoppe from '@assets/Shoppe.svg'
+import profile from '@assets/Profile.svg'
+import cart from '@assets/Cart.svg'
+import search from '@assets/Search.svg'
+
+export {search, cart, profile, Shoppe}
--- src/components/Narbar/Navbar.tsx
+++ src/components/Narbar/Navbar.tsx
@@ -1,7 +1,8 @@
-import Shoppe from '@assets/Shoppe.svg'
-import Profile from '@assets/Profile.svg'
-import Cart from '@assets/Cart.svg'
-import Search from '@assets/Search.svg'
+// import Shoppe from '@assets/Shoppe.svg'
+// import profile from '@assets/Profile.svg'
+// import cart from '@assets/Cart.svg'
+// import search from '@assets/Search.svg'
+import {search, cart, profile, Shoppe} from '@assets/'
 import './navbar.scss'
 
 export const Navbar = () => {
@@ -11,16 +12,16 @@ export const Navbar = () => {
         <img src={Shoppe} alt="Shoope icon" />
       </div>
       <div className="navbar__menu">
-        <div className="navbar__item navbar__links">
-          <div>Shop</div>
-          <div>Blog</div>
-          <div>Our Story</div>
+        <div className="navbar__list navbar__links">
+          <div className="navbar__item">Shop</div>
+          <div className="navbar__item">Blog</div>
+          <div className="navbar__item">Our Story</div>
         </div>
         <div className="navbar__separator"></div>
-        <div className="navbar__item navbar__icons">
-          <img src={Search} alt="Search icon" />
-          <img src={Cart} alt="Cart icon" />
-          <img src={Profile} alt="Profile icon" />
+        <div className="navbar__item--menu navbar__icons">
+          <img src={search} alt="Search icon" />
+          <img src={cart} alt="Cart icon" />
+          <img src={profile} alt="Profile icon" />
         </div>
       </div>
     </div>
