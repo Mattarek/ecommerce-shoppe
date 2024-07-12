87a844ce33688b6061e082307f5a5d3d5e2d4e59
Marcin Barć <marb637@gmail.com>
2024-07-12 14:02:12 +0200

    Add: Default router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { Layout } from './layout/layout'
 import './styles/index.scss'
 
-function App() {
+export function App() {
   return (
     <Layout>
       <div></div>
     </Layout>
   )
 }
-
-export default App
