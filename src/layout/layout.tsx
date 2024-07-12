03515f0986c7710589fa26fe4472cfecc8973004
Marcin Barć <marb637@gmail.com>
2024-07-12 15:55:30 +0200

    Change Layout position change

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -7,7 +7,15 @@ interface Props {
 
 export const Layout = ({ children }: Props) => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', margin: '3vh 5vw' }}>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        margin: '3vh 5vw',
+        maxHeight: '100vh',
+        maxWidth: '100vw'
+      }}
+    >
       <Navbar />
       {children}
       <div style={{ display: 'block', height: '60vh', width: '100%' }}></div>
