4e76e618b3997a208ac6ad272a84b386ee2ee4de
Mattarek <marb637@gmail.com>
2024-07-21 16:41:13 +0200

    I18Next configuration added to repository (#4)

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import './i18n.js'
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
